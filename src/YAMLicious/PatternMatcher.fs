63416ede6bd5c22aff8459dca10943ae880cb091
Caroline Ott <caroline.ott1994@gmail.com>
2024-06-05 17:04:57 +0200

    add basic patterns

---
+++ src/YAMLicious/PatternMatcher.fs
@@ -0,0 +1,21 @@
+﻿module YAMLicious.Persil
+
+open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
+
+[<Literal>]
+let KeyValuePattern = 
+    "^.*?: .*"
+
+[<Literal>]
+let ValuePattern = 
+    "^(?!.*:)\S.*"
+
+[<Literal>]
+let SequencePattern = 
+    "^-(?![\[\]])\s.*"
+
+[<Literal>]
+let NewLineChar = '\n'
