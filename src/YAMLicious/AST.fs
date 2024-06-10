1d4aeb8ab042dcc223d74bb6b4f1bd8b36eda74f
Caroline Ott <caroline.ott1994@gmail.com>
2024-06-10 15:56:14 +0200

    reorder functions

--- src/YAMLicious/AST.fs
+++ src/YAMLicious/AST.fs
@@ -29,48 +29,6 @@ and YAMLASTElement =
     | Intendation of YAMLASTElement list
     | Line of string
 
-    static member write(rootElement:YAMLASTElement, ?fconfig: Config -> Config) =
-        let config = Config.init() |> fun config -> if fconfig.IsSome then fconfig.Value config else config
-        let sb = new StringBuilder()
-        let rec loop (current: YAMLASTElement) (sb: StringBuilder) (config: Config) =
-            match current with
-            | Line line ->
-                sb.AppendLine(config.WhitespaceString+line) |> ignore
-            | Intendation children ->
-                let nextConfig = {config with Level = config.Level + 1}
-                for child in children do
-                    loop child sb nextConfig
-            | Level children ->
-                for child in children do
-                    loop child sb config
-        loop rootElement sb config
-        sb.ToString()
-
-    static member read(yamlStr: string) =
-        let content = Persil.pipeline yamlStr
-        let rec loop (lines: string list) (currentIntendation: int) (acc: YAMLASTElement list) =
-            match lines with
-            | [] -> acc
-            | line::rest ->
-                let lineEle = Line (line.Trim())
-                let nextIntendation = ReadHelpers.indentLevel line
-                if nextIntendation = currentIntendation then
-                    loop rest currentIntendation (Line (line.Trim())::acc)
-                else
-                    let nextLevelLines = rest |> List.takeWhile (fun l -> ReadHelpers.indentLevel l > currentIntendation)
-                    let currentLevelLines = rest |> List.skipWhile (fun l -> ReadHelpers.indentLevel l > currentIntendation)
-                    let otherChildren = loop nextLevelLines nextIntendation [] |> List.rev
-                    let children = lineEle::otherChildren
-                    loop currentLevelLines currentIntendation (Intendation children::acc)
-        let ast = 
-            loop (List.ofArray content.Lines) 0 [] 
-            |> List.rev
-            |> Level
-        {
-            AST = ast
-            StringMap = content.StringMap
-            CommentMap = content.CommentMap
-        }
 
     override this.ToString() =
         let sb = StringBuilder()
@@ -91,14 +49,54 @@ and YAMLASTElement =
         innerprint this 0
         sb.ToString()
             
+let write(rootElement:YAMLASTElement, fconfig: (Config -> Config) option) =
+    let config = Config.init() |> fun config -> if fconfig.IsSome then fconfig.Value config else config
+    let sb = new StringBuilder()
+    let rec loop (current: YAMLASTElement) (sb: StringBuilder) (config: Config) =
+        match current with
+        | Line line ->
+            sb.AppendLine(config.WhitespaceString+line) |> ignore
+        | Intendation children ->
+            let nextConfig = {config with Level = config.Level + 1}
+            for child in children do
+                loop child sb nextConfig
+        | Level children ->
+            for child in children do
+                loop child sb config
+    loop rootElement sb config
+    sb.ToString()
+
+let read(yamlStr: string) =
+    let content = Persil.pipeline yamlStr
+    let rec loop (lines: string list) (currentIntendation: int) (acc: YAMLASTElement list) =
+        match lines with
+        | [] -> acc
+        | line::rest ->
+            let lineEle = Line (line.Trim())
+            let nextIntendation = ReadHelpers.indentLevel line
+            if nextIntendation = currentIntendation then
+                loop rest currentIntendation (Line (line.Trim())::acc)
+            else
+                let nextLevelLines = rest |> List.takeWhile (fun l -> ReadHelpers.indentLevel l > currentIntendation)
+                let currentLevelLines = rest |> List.skipWhile (fun l -> ReadHelpers.indentLevel l > currentIntendation)
+                let otherChildren = loop nextLevelLines nextIntendation [] |> List.rev
+                let children = lineEle::otherChildren
+                loop currentLevelLines currentIntendation (Intendation children::acc)
+    let ast = 
+        loop (List.ofArray content.Lines) 0 [] 
+        |> List.rev
+        |> Level
+    {
+        AST = ast
+        StringMap = content.StringMap
+        CommentMap = content.CommentMap
+    }
         
 let mkLine (line:string) = Line line
 
 let mklLevel (children: #seq<YAMLASTElement>) = List.ofSeq children |> Level
 
 let mkIntendation (children: #seq<YAMLASTElement>) = List.ofSeq children |> Intendation
-
-let write rootElement = YAMLASTElement.write(rootElement)
 
 type IYAMLConvertible =
     abstract ToYAMLAst: unit -> YAMLAST list
