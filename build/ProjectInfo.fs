aa210b9a7b187f301cda436953642341b2cde92c
Kevin F <Freymaurer@gmx.de>
2025-12-16 12:40:19 +0100

    update version

--- build/ProjectInfo.fs
+++ build/ProjectInfo.fs
@@ -21,7 +21,7 @@ let ProjectNamePython = "YAMLicious"
 let ProjectNameJavaScript = "YAMLicious"
 
 [<Literal>]
-let Version = "0.0.3"
+let Version = "1.0.0-alpha.1"
 
 [<Literal>]
 let PyprojectTOML = "pyproject.toml"
