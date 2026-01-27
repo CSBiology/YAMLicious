4f9a10b2118d5d40749134de68cd0f787b3b9d33
caroott <caroline.ott1994@gmail.com>
2026-01-27 14:20:41 +0100

    fix interop string and int check

--- src/YAMLicious/Interop/JsInterop.fs
+++ src/YAMLicious/Interop/JsInterop.fs
@@ -6,9 +6,9 @@ open Fable.Core.JsInterop
 
 let isInt (s:string): bool =
     emitJsStatement (s) """if (typeof $0 != "string") return false // we only process strings!  
-                return !isNaN($0) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+                return !isNaN(parseFloat($0)) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
                        !isNaN(parseFloat($0)) // ...and ensure strings of whitespace fail"""
 
 let isString (s:string): bool =
-    emitJsStatement s """return typeof $0 === 'string' || value instanceof String"""
+    emitJsStatement s """return typeof $0 === 'string' || $0 instanceof String"""
 #endif
